2757d3a7581c23869474ac539569eb9af80fce34
perry <perrypengwang@gmail.com>
2026-08-18 15:34:15 +0800

    feat(ui): 版本信息对话框 GitHub 链接支持点击跳转

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -23,16 +23,10 @@ MainWindow::MainWindow(QWidget *parent)
 
     // 版本信息
     connect(ui->actionUpdateInfo, &QAction::triggered, this, [this](){
-        QMessageBox::information(nullptr, "版本信息", "当前版本：" APP_VERSION "，Created By Perry.\n"
-                                 "0.6版本更新日志：\n"
-                                 "1. crc计算器基本实现\n"
-                                 "0.5版本更新日志：\n"
-                                 "1. Fix Some Bugs\n"
-                                 "2. ASCII转换器：不可显示字符添加橙色背景显示\n"
-                                 "0.4版本更新日志：\n"
-                                 "1. 添加图标, 优化不同分辨率缩放的显示\n"
-                                 "2. ASCII转换器：不可显示字符添加方框显示\n"
-                                 "3. 时间戳转换器：基本完善");
+        QMessageBox::information(nullptr, "版本信息",
+            QString("当前版本：%1，Created By Perry.<br>"
+                    "GitHub: <a href=\"https://github.com/huanfenz/PerryBox\">https://github.com/huanfenz/PerryBox</a>")
+                .arg(APP_VERSION));
     });
 }
 
